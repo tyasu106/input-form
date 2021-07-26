367a9acc1f14581e5f5e936639a72ff18eb7c4f3
tyasu106 <t.yasu106@gmail.com>
2021-07-26 13:43:34 +0900

    設問表示切り替え

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -3,65 +3,60 @@ import { Link } from "react-router-dom";
 
 export const Step2 = () => {
   const [val, setVal] = useState("");
-  const selectInsurance = (e) => {
-    setVal(e.target.value);
-    if (e.target.value === "no") {
-    }
+
+  const que1 = {
+    id: 0,
+    question: "現在、生命保険に加入されていますか？",
+    state: false,
   };
-  const selectYesNo = (
-    <div>
-      {" "}
-      <label>
-        <input
-          type="radio"
-          value="yes"
-          onChange={selectInsurance}
-          checked={val === "yes"}
-        />
-        はい
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="no"
-          onChange={selectInsurance}
-          checked={val === "no"}
-        />
-        いいえ
-      </label>
-    </div>
-  );
-  const questions = [
-    {
-      id: 0,
-      question: "現在、生命保険に加入されていますか？",
-      answer: selectYesNo,
-    },
-    {
-      id: 1,
-      question: "現在入院中ですか。または、最近３ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？",
-      answer: selectYesNo,
-    },
-    {
-      id: 2,
-      question: "過去5年以内に、病気やけがで、手術をうけたことまたは継続して７日以上の入院をしたことがありますか？",
-      answer: selectYesNo,
-    },
-  ];
+  const que2 = {
+    id: 1,
+    question:
+      "現在入院中ですか。または、最近３ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？",
+    state: false,
+  };
+  const que3 = {
+    id: 2,
+    question:
+      "過去5年以内に、病気やけがで、手術をうけたことまたは継続して７日以上の入院をしたことがありますか？",
+    state: false,
+  };
+
+  const selectInsurance = (e) => {
+    if (e.target.value === "q1_no") {
+      setVal(e.target.value);
+      que1.state = true;
+    } else {
+      setVal(e.target.value);
+      que1.state = false;
+    }
+    console.log(que1.state);
+  };
   return (
     <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
       <div className="bg-white p-8 w-80 reunded">
-        <div>以下にお答え下さい</div>
         <hr />
-        {questions.map((q) => {
-          return (
-            <div>
-              {q.question}
-              {q.answer}
-              <br/>
-            </div>
-          );
-        })}
+        {que1.question}
+        <br />
+        <label>
+          <input
+            type="radio"
+            value="q1_yes"
+            onChange={selectInsurance}
+            checked={val === "q1_yes"}
+          ></input>
+          はい
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="q1_no"
+            onChange={selectInsurance}
+            checked={val === "q1_no"}
+          ></input>
+          いいえ
+        </label>
+        {que1.state && <div>{que2.question}</div>}
       </div>
       <Link to="/">前へ戻る</Link>
       <Link to="/step3">次へ進む</Link>
