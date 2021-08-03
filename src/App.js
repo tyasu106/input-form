74886e4bdf55e1f1248614db80e97aaaefb0ed61
tyasu106 <t.yasu106@gmail.com>
2021-08-03 20:42:55 +0900

    生年月日ループ処理

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Step1 } from "./components/Step1";
 import { Step2 } from "./components/Step2";
 import { Step3 } from "./components/Step3";
+import { example } from "./components/example";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Route exact path='/' component={Step1} />
       <Route path='/step2' component={Step2} />
       <Route path='/step3' component={Step3} />
+      <Route path='/example' component={example} />
     </Switch>
   </BrowserRouter>
   );
--- src/components/Step1.js
+++ src/components/Step1.js
@@ -5,6 +5,84 @@ export const Step1 = () => {
   const [val, setVal] = useState("");
   const selectSex = (e) => setVal(e.target.value);
 
+  const years = [
+    { value: "1991", year: "1991年(平成3年)" },
+    { value: "1992", year: "1992年(平成4年)" },
+    { value: "1993", year: "1993年(平成5年)" },
+    { value: "1994", year: "1994年(平成6年)" },
+    { value: "1995", year: "1995年(平成7年)" },
+    { value: "1996", year: "1996年(平成8年)" },
+    { value: "1997", year: "1997年(平成9年)" },
+    { value: "1998", year: "1998年(平成10年)" },
+    { value: "1999", year: "1999年(平成11年)" },
+    { value: "2000", year: "2000年(平成12年)" },
+    { value: "2001", year: "2001年(平成13年)" },
+    { value: "2002", year: "2002年(平成14年)" },
+    { value: "2003", year: "2003年(平成15年)" },
+    { value: "2004", year: "2004年(平成16年)" },
+    { value: "2005", year: "2005年(平成17年)" },
+    { value: "2006", year: "2006年(平成18年)" },
+    { value: "2007", year: "2007年(平成19年)" },
+    { value: "2008", year: "2008年(平成20年)" },
+    { value: "2009", year: "2009年(平成21年)" },
+    { value: "2010", year: "2010年(平成22年)" },
+    { value: "2011", year: "2011年(平成23年)" },
+    { value: "2012", year: "2012年(平成24年)" },
+    { value: "2013", year: "2013年(平成25年)" },
+    { value: "2014", year: "2014年(平成26年)" },
+    { value: "2015", year: "2015年(平成27年)" },
+    { value: "2016", year: "2016年(平成28年)" },
+    { value: "2017", year: "2017年(平成29年)" },
+    { value: "2018", year: "2018年(平成30年)" },
+    { value: "2019", year: "2019年(平成31年)" },
+  ];
+  const months = [
+    { value: "1", month: "1月" },
+    { value: "2", month: "2月" },
+    { value: "3", month: "3月" },
+    { value: "4", month: "4月" },
+    { value: "5", month: "5月" },
+    { value: "6", month: "6月" },
+    { value: "7", month: "7月" },
+    { value: "8", month: "8月" },
+    { value: "9", month: "9月" },
+    { value: "10", month: "10月" },
+    { value: "11", month: "11月" },
+    { value: "12", month: "12月" },
+  ];
+  const days = [
+    { value: "1", day: "1日" },
+    { value: "2", day: "2日" },
+    { value: "3", day: "3日" },
+    { value: "4", day: "4日" },
+    { value: "5", day: "5日" },
+    { value: "6", day: "6日" },
+    { value: "7", day: "7日" },
+    { value: "8", day: "8日" },
+    { value: "9", day: "9日" },
+    { value: "10", day: "10日" },
+    { value: "11", day: "11日" },
+    { value: "12", day: "12日" },
+    { value: "13", day: "13日" },
+    { value: "14", day: "14日" },
+    { value: "15", day: "15日" },
+    { value: "16", day: "16日" },
+    { value: "17", day: "17日" },
+    { value: "18", day: "18日" },
+    { value: "19", day: "19日" },
+    { value: "20", day: "20日" },
+    { value: "21", day: "21日" },
+    { value: "22", day: "22日" },
+    { value: "23", day: "23日" },
+    { value: "24", day: "24日" },
+    { value: "25", day: "25日" },
+    { value: "26", day: "26日" },
+    { value: "27", day: "27日" },
+    { value: "28", day: "28日" },
+    { value: "29", day: "29日" },
+    { value: "30", day: "30日" },
+    { value: "31", day: "31日" },
+  ];
   return (
     <div className="min-h-screen text-gray-600 text-sm font-mono">
       <div className="bg-white  p-8 w-full reunded">
@@ -33,82 +111,19 @@ export const Step1 = () => {
           <br />
           <div>-生年月日-</div>
           <select name="year">
-            <option value="1991">1991年(平成3年)</option>
-            <option value="1992">1992年(平成4年)</option>
-            <option value="1993">1993年(平成5年)</option>
-            <option value="1994">1994年(平成6年)</option>
-            <option value="1995">1995年(平成7年)</option>
-            <option value="1996">1996年(平成8年)</option>
-            <option value="1997">1997年(平成9年)</option>
-            <option value="1998">1998年(平成10年)</option>
-            <option value="1999">1999年(平成11年)</option>
-            <option value="2000">2000年(平成12年)</option>
-            <option value="2001">2001年(平成13年)</option>
-            <option value="2002">2002年(平成14年)</option>
-            <option value="2003">2003年(平成15年)</option>
-            <option value="2004">2004年(平成16年)</option>
-            <option value="2005">2005年(平成17年)</option>
-            <option value="2006">2006年(平成18年)</option>
-            <option value="2007">2007年(平成19年)</option>
-            <option value="2008">2008年(平成20年)</option>
-            <option value="2009">2009年(平成21年)</option>
-            <option value="2010">2010年(平成22年)</option>
-            <option value="2011">2011年(平成23年)</option>
-            <option value="2012">2012年(平成24年)</option>
-            <option value="2013">2013年(平成25年)</option>
-            <option value="2014">2014年(平成26年)</option>
-            <option value="2015">2015年(平成27年)</option>
-            <option value="2016">2016年(平成28年)</option>
-            <option value="2017">2017年(平成29年)</option>
-            <option value="2018">2018年(平成30年)</option>
-            <option value="2019">2019年(平成31年)</option>
+            {years.map((year) => {
+              return <option value={year.value}>{year.year}</option>;
+            })}
           </select>
           <select name="month">
-            <option value="1">1月</option>
-            <option value="2">2月</option>
-            <option value="3">3月</option>
-            <option value="4">4月</option>
-            <option value="5">5月</option>
-            <option value="6">6月</option>
-            <option value="7">7月</option>
-            <option value="8">8月</option>
-            <option value="9">9月</option>
-            <option value="10">10月</option>
-            <option value="11">11月</option>
-            <option value="12">12月</option>
+            {months.map((month) => {
+              return <option value={month.value}>{month.month}</option>;
+            })}
           </select>
           <select name="day">
-            <option value="1">1日</option>
-            <option value="2">2日</option>
-            <option value="3">3日</option>
-            <option value="4">4日</option>
-            <option value="5">5日</option>
-            <option value="6">6日</option>
-            <option value="7">7日</option>
-            <option value="8">8日</option>
-            <option value="9">9日</option>
-            <option value="10">10日</option>
-            <option value="11">11日</option>
-            <option value="12">12日</option>
-            <option value="13">13日</option>
-            <option value="14">14日</option>
-            <option value="15">15日</option>
-            <option value="16">16日</option>
-            <option value="17">17日</option>
-            <option value="18">18日</option>
-            <option value="19">19日</option>
-            <option value="20">20日</option>
-            <option value="21">21日</option>
-            <option value="22">22日</option>
-            <option value="23">23日</option>
-            <option value="24">24日</option>
-            <option value="25">25日</option>
-            <option value="26">26日</option>
-            <option value="27">27日</option>
-            <option value="28">28日</option>
-            <option value="29">29日</option>
-            <option value="30">30日</option>
-            <option value="31">31日</option>
+            {days.map((day) => {
+              return <option value={day.value}>{day.day}</option>;
+            })}
           </select>
         </div>
       </div>
